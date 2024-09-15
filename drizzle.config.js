2c9cdf92ee23708374e9aae0ad15e009866846a8
KunalKaushikk <kunalkaushik11786@gmail.com>
2024-09-15 22:10:22 +0530

    Initial Commit

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+export default {
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: "postgresql://finance-tracker_owner:Oikgwd0R7Wen@ep-long-shadow-a1ehapup.ap-southeast-1.aws.neon.tech/finance-tracker?sslmode=require",
+    connectionString:
+      "postgresql://finance-tracker_owner:Oikgwd0R7Wen@ep-long-shadow-a1ehapup.ap-southeast-1.aws.neon.tech/finance-tracker?sslmode=require",
+  },
+};
